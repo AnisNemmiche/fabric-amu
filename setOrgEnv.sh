a1dda06325fe4271836e53242263b04ead30edfc
AnisNemmiche <anis.nemmiche@etu.univ-amu.fr>
2025-08-21 19:14:17 +0200

    first commit

---
+++ setOrgEnv.sh
@@ -0,0 +1,59 @@
+#!/usr/bin/env bash
+#
+# SPDX-License-Identifier: Apache-2.0
+
+
+
+
+# default to using Manufacturer
+ORG=${1:-Manufacturer}
+
+# Exit on first error, print all commands.
+set -e
+set -o pipefail
+
+# Where am I?
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )/.." && pwd )"
+
+ORDERER_CA=${DIR}/test-network/organizations/ordererOrganizations/amu.local/tlsca/tlsca.amu.local-cert.pem
+PEER0_ORG1_CA=${DIR}/test-network/organizations/peerOrganizations/manufacturer.amu.local/tlsca/tlsca.manufacturer.amu.local-cert.pem
+PEER0_ORG2_CA=${DIR}/test-network/organizations/peerOrganizations/subcontractor.amu.local/tlsca/tlsca.subcontractor.amu.local-cert.pem
+# PEER0_ORG3_CA=${DIR}/test-network/organizations/peerOrganizations/org3.amu.local/tlsca/tlsca.org3.amu.local-cert.pem
+
+
+if [[ ${ORG,,} == "manufacturer" || ${ORG,,} == "digibank" ]]; then
+
+   CORE_PEER_LOCALMSPID=ManufacturerMSP
+   CORE_PEER_MSPCONFIGPATH=${DIR}/test-network/organizations/peerOrganizations/manufacturer.amu.local/users/Admin@manufacturer.amu.local/msp
+   CORE_PEER_ADDRESS=localhost:7051
+   CORE_PEER_TLS_ROOTCERT_FILE=${DIR}/test-network/organizations/peerOrganizations/manufacturer.amu.local/tlsca/tlsca.manufacturer.amu.local-cert.pem
+
+elif [[ ${ORG,,} == "subcontractor" || ${ORG,,} == "magnetocorp" ]]; then
+
+   CORE_PEER_LOCALMSPID=SubcontractorMSP
+   CORE_PEER_MSPCONFIGPATH=${DIR}/test-network/organizations/peerOrganizations/subcontractor.amu.local/users/Admin@subcontractor.amu.local/msp
+   CORE_PEER_ADDRESS=localhost:9051
+   CORE_PEER_TLS_ROOTCERT_FILE=${DIR}/test-network/organizations/peerOrganizations/subcontractor.amu.local/tlsca/tlsca.subcontractor.amu.local-cert.pem
+
+else
+   echo "Unknown \"$ORG\", please choose Manufacturer/Digibank or Subcontractor/Magnetocorp"
+   echo "For example to get the environment variables to set upa Subcontractor shell environment run:  ./setOrgEnv.sh Subcontractor"
+   echo
+   echo "This can be automated to set them as well with:"
+   echo
+   echo 'export $(./setOrgEnv.sh Subcontractor | xargs)'
+   exit 1
+fi
+
+# output the variables that need to be set
+echo "CORE_PEER_TLS_ENABLED=true"
+echo "ORDERER_CA=${ORDERER_CA}"
+echo "PEER0_ORG1_CA=${PEER0_ORG1_CA}"
+echo "PEER0_ORG2_CA=${PEER0_ORG2_CA}"
+echo "PEER0_ORG3_CA=${PEER0_ORG3_CA}"
+
+echo "CORE_PEER_MSPCONFIGPATH=${CORE_PEER_MSPCONFIGPATH}"
+echo "CORE_PEER_ADDRESS=${CORE_PEER_ADDRESS}"
+echo "CORE_PEER_TLS_ROOTCERT_FILE=${CORE_PEER_TLS_ROOTCERT_FILE}"
+
+echo "CORE_PEER_LOCALMSPID=${CORE_PEER_LOCALMSPID}"
